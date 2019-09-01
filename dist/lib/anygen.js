fa7c691a9f7683f915599dd7182bffd42daad169
ma jerez <jerez.m82@gmail.com>
2019-09-01 14:31:40 +0100

    remove unused files and unused dependencies.

--- dist/lib/anygen.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-var Bl = require("./Blueprint");
-var Bu = require("./Builder");
-var UT = require("./utils");
-exports.Blueprint = Bl.Blueprint;
-exports.Builder = Bu.Builder;
-exports.utils = UT;
-//# sourceMappingURL=anygen.js.map
